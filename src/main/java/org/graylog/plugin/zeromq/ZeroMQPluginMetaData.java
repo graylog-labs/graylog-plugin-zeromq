0e71899ebe35475a7da053df7f6c5ed4bad63c85
Bernd Ahlers <bernd@graylog.com>
2015-09-10 10:59:48 +0200

    Bump version in metadata

--- src/main/java/org/graylog/plugin/zeromq/ZeroMQPluginMetaData.java
+++ src/main/java/org/graylog/plugin/zeromq/ZeroMQPluginMetaData.java
@@ -31,7 +31,7 @@ public class ZeroMQPluginMetaData implements PluginMetaData {
 
     @Override
     public Version getVersion() {
-        return new Version(1, 1, 1);
+        return new Version(1, 1, 2);
     }
 
     @Override
